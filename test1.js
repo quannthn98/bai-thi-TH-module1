3158a0133f2f6519503794cf54882d47de2218c8
x-Beo-x <86937244+x-Beo-x@users.noreply.github.com>
2021-08-02 10:10:13 +0700

    test 1

---
+++ test1.js
@@ -0,0 +1,17 @@
+let root;
+
+function check(a, b) {
+    if (a === 0) {
+        document.write('This equation is unSolvable');
+    } else {
+        if (b === 0) {
+            document.write('This equation has unlimited Root')
+        } else {
+            root = (-b) / a
+            document.write('This equation has 1 root is: ' + root)
+        }
+    }
+}
+
+check(5, 8)
+
---
+++ test3.js
@@ -0,0 +1,22 @@
+let inputString = prompt('Please input any string');
+let vowels = ['a', 'o', 'e', 'i', 'u'];
+let count = 0;
+let stringsVowel = [];
+
+function checkVowel(string) {
+    for (let i = 0; i < string.length; i++) {
+        for (let j = 0; j < vowels.length; j++) {
+            if (string[i] === vowels[j]) {
+                stringsVowel.push(string[i]);
+                count++;
+            }
+        }
+    }
+    if (stringsVowel.length == 0) {
+        document.write('Your input string: ' + '"' + string + '"' + ' dont contain any vowel')
+    } else {
+        document.write('Your input string: ' + '"' + string + '"' + ' have vowel(s) are ' + stringsVowel)
+    }
+}
+
+checkVowel(inputString);
